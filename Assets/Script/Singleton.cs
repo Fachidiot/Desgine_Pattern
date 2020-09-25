685dc238898c1b35be33c786b3f5f3fce1370f55
fachidiot <jerrykim428@gmail.com>
2020-09-25 16:46:04 +0900

    Singleton Pattern

---
+++ Assets/Script/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Singleton
+{
+    private static Singleton m_Instance;
+
+    public static Singleton Instance()
+    {
+        if(m_Instance == null)
+        {
+            m_Instance = new Singleton();
+        }
+
+        // 다중 쓰레드일 경우
+        if(m_Instance == null)
+        {
+            lock(m_Instance)
+            {
+                m_Instance = new Singleton();
+            }
+        }
+
+        return m_Instance;
+    }
+}
